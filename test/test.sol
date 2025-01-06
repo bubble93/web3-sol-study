73682df09d6c71803e4dd3c2ca7b126cf6a76b66
bubble93 <17791205330@163.com>
2025-01-06 08:11:41 +0800

    change mapping

--- test/test.sol
+++ test/test.sol
@@ -13,20 +13,18 @@ contract HelloWorld {
         address addr;
     }
 
-    Info[] infos;
+    mapping(uint256 => Info) infoMapping;
 
     function sayHello(uint256 _id) public view returns(string memory) {
-        for(uint256 i = 0; i < infos.length; i++) {
-            if (infos[i].id == _id) {
-                return addinfo(infos[i].phrase);
-            }
+        if (infoMapping[_id].addr != address(0x0)) {
+            return addinfo(infoMapping[_id].phrase);
         }
         return addinfo(strVar);
     }
 
     function setHelloWorld(string memory newString, uint256 _id) public {
         Info memory info = Info(newString, _id, msg.sender);
-        infos.push(info);
+        infoMapping[_id] = info;
     }
     /**
         1. storage - 永久
